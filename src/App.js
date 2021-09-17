c2e1e8f4d8c732b5972cd00ed93908fe0cdeeb29
danlaurie <danlaurie3@gmail.com>
2021-09-17 22:57:30 +0100

    favicon added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1>Super Standings</h1>
+      <p>A simple app that displays Formula 1 information past and present!</p>
+    </>
+  )
 }
 
 export default App
