0c03976bd1973f77c3c127023242036f4017662e
danlaurie <danlaurie3@gmail.com>
2021-09-17 23:16:03 +0100

    recieves current standings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 function App() {
+
+  useEffect(() => {
+    const getStandings = async () => {
+      try {
+        const { data } = await axios('http://ergast.com/api/f1/current/driverStandings.json')
+        console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getStandings()
+  }, [])
+
+
+
   return (
     <>
       <h1>Super Standings</h1>
