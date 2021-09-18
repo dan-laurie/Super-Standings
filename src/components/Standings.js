f9503c7c40e5d5580b55d3b9ea8972d031daef6e
danlaurie <danlaurie3@gmail.com>
2021-09-18 17:04:51 +0100

    loading spinner added

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import FilterYears from './FilterYears'
+import spinner from '../images/loading-buffering.gif'
 
 
 const Standings = () => {
@@ -8,21 +9,26 @@ const Standings = () => {
   const [ standings, setStandings ] = useState([])
   const [ filteredYears, setFilteredYears ] = useState([])
   const [ rounds, setRounds ] = useState([])
+  const [ hasError, setHasError ] = useState(false)
 
   useEffect(() => {
+    setStandings(null)
+    setRounds(null)
     const getStandings = async () => {
       try {
         const { data } = await axios('https://ergast.com/api/f1/current/driverStandings.json')
         setStandings(data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
         setRounds(data.MRData.StandingsTable.StandingsLists[0].round)
       } catch (err) {
-        console.log(err)
+        setHasError(true)
       }
     }
     getStandings()
   }, [filteredYears])
 
   const handleFilters = async (e) => {
+    setStandings(null)
+    setRounds(null)
     if (e.target.value === '') { //if no user input, display all 
       setFilteredYears(standings)
     } else {
@@ -37,30 +43,43 @@ const Standings = () => {
       <div className="background">
         <FilterYears handleFilters={handleFilters}/>
         <h6>After {rounds} rounds</h6>
-        <div className="table-div">
-          <table className="table">
-            <>
-              <thead>
-                <tr>
-                  <th scope="col">Pos</th>
-                  <th scope="col">Driver</th>
-                  <th scope="col">Team</th>
-                  <th scope="col">Points</th>
-                </tr>
-              </thead>
-              {(filteredYears.length > 0 ? filteredYears : standings).map((s, i) => {
-                return (
-                  <tr key={i}>
-                    <td scope="row"><strong>{s.position}</strong></td>
-                    <td>{s.Driver.givenName} {s.Driver.familyName}</td>
-                    <td>{s.Constructors[0].name}</td>
-                    <td>{s.points}</td>
-                  </tr>       
-                )
-              })}
-            </>
-          </table>
-        </div>
+        {standings ?
+          <div className="table-div">
+            <table className="table">
+              <>
+                <thead>
+                  <tr>
+                    <th scope="col">Pos</th>
+                    <th scope="col">Driver</th>
+                    <th scope="col">Team</th>
+                    <th scope="col">Points</th>
+                  </tr>
+                </thead>
+                {(filteredYears.length > 0 ? filteredYears : standings).map((s, i) => {
+                  return (
+                    <tr key={i}>
+                      <td scope="row"><strong>{s.position}</strong></td>
+                      <td>{s.Driver.givenName} {s.Driver.familyName}</td>
+                      <td>{s.Constructors[0].name}</td>
+                      <td>{s.points}</td>
+                    </tr>       
+                  )
+                })}
+              </>
+            </table>
+          </div>
+          :
+          <>
+            {hasError ?
+
+              <h1>Oops! Something went wrong</h1>
+              :
+              <div className="spinner">
+                <img src={spinner}/>
+              </div>
+            }
+          </>
+        } 
       </div>
     
     </>
