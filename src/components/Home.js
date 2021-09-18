13e8405bf3f27785192ee2c1f90fd3ac8692da42
danlaurie <danlaurie3@gmail.com>
2021-09-18 16:16:51 +0100

    css adjustments

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,11 @@ const Home = () => {
   return (
     <div className="background-home">
       <div className="container">
-        <h1 className="title">Super Standings.</h1>
+        <h1 className="title">Super Standings.</h1>       
         <p className="intro">A simple and easy to use app to find the current or past Driver&apos;s standings of the FIA Formula One World Championship.</p>
-        <img className="lewis"src={lewis} alt="lewis-hamilton" />
+        <div className="lewis-div">
+          <img className="lewis"src={lewis} alt="lewis-hamilton" />
+        </div>
         <Link className="home-btn" to="/standings">View Standings</Link>
       </div>
     </div>
